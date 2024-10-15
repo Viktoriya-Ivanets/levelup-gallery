dbdce737d9e851c813a5055b4b4fe4c275d89ed4
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-15 14:37:10 +0300

    grid_layout.php - Create grid layout for photo displaying

---
+++ templates/layouts/grid_layout.php
@@ -0,0 +1,8 @@
+<div class="gallery-grid" id="gallery-grid">
+    <?php $images = getImages(); ?>
+    <?php foreach ($images as $image): ?>
+        <a href="<?= $image['full'] ?>" data-pswp-width="<?= $image['width'] ?>" data-pswp-height="<?= $image['height'] ?>">
+            <img src="<?= $image['thumb'] ?>" alt="Gallery image">
+        </a>
+    <?php endforeach; ?>
+</div>
