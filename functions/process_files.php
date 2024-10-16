30bc57db72f88e0c33873772ebd96cde363fc88e
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-16 15:57:43 +0300

    process_files.php - Refactore functions

--- functions/process_files.php
+++ functions/process_files.php
@@ -7,23 +7,36 @@
  */
 function getImages(string $directory = 'assets/images'): array
 {
+    $allowedExtensions = ['jpg', 'jpeg', 'png'];
+    $files = getFilesFromDirectory($directory);
+
+    return filterImages($files, $allowedExtensions, $directory);
+}
+
+/**
+ * Get files from directory
+ * @param string $directory
+ * @return array
+ */
+function getFilesFromDirectory(string $directory): array
+{
+    return is_dir($directory) ? scandir($directory) : [];
+}
+
+/**
+ * Filter and format image files
+ * @param array $files
+ * @param array $allowedExtensions
+ * @param string $directory
+ * @return array
+ */
+function filterImages(array $files, array $allowedExtensions, string $directory): array
+{
     $images = [];
-    $allowed_extensions = ['jpg', 'jpeg', 'png'];
 
-    if (is_dir($directory)) {
-        $files = scandir($directory);
-
-        foreach ($files as $file) {
-            if ($file != '.' && $file != '..') {
-                if (in_array(getExtension($file), $allowed_extensions)) {
-                    $images[] = [
-                        'full' => $directory . '/' . $file,
-                        'thumb' => $directory . '/' . $file,
-                        'width' => 1200,
-                        'height' => 700
-                    ];
-                }
-            }
+    foreach ($files as $file) {
+        if (!checkExtension($file, $allowedExtensions)) {
+            $images[] = formatImageData($file, $directory);
         }
     }
 
@@ -31,20 +44,50 @@ function getImages(string $directory = 'assets/images'): array
 }
 
 /**
+ * Format image data
+ * @param string $file
+ * @param string $directory
+ * @return array
+ */
+function formatImageData(string $file, string $directory): array
+{
+    return [
+        'full' => $directory . '/' . $file,
+        'thumb' => $directory . '/' . $file,
+        'width' => 1200,
+        'height' => 700
+    ];
+}
+
+/**
  * Save uploaded images in directory
  * @param array $files
  * @param string $directory
- * @return string[]
+ * @return void
  */
-function uploadImages(array $files, string $directory = '../assets/images'): array
+function uploadImages(array $files, string $directory = '../assets/images'): void
 {
-    $uploadedFiles = [];
     if (!is_dir($directory)) {
         mkdir($directory, 0755, true);
     }
 
+    $uploadedFiles = processFileUploads($files, $directory);
+
+    setUploadMessages($uploadedFiles);
+}
+
+/**
+ * Process file uploads
+ * @param array $files
+ * @param string $directory
+ * @return array
+ */
+function processFileUploads(array $files, string $directory): array
+{
+    $uploadedFiles = [];
+
     foreach ($files as $file) {
-        $uniqueName = uniqid('img_', true) . '.' . getExtension($file['name']);
+        $uniqueName = generateUniqueFileName($file['name']);
         $targetFile = $directory . '/' . $uniqueName;
 
         if (move_uploaded_file($file['tmp_name'], $targetFile)) {
@@ -53,4 +96,14 @@ function uploadImages(array $files, string $directory = '../assets/images'): arr
     }
 
     return $uploadedFiles;
+}
+
+/**
+ * Generate unique file name
+ * @param string $fileName
+ * @return string
+ */
+function generateUniqueFileName(string $fileName): string
+{
+    return uniqid('img_', true) . '.' . getExtension($fileName);
 }
