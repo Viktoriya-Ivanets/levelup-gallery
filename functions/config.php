6609d0500639eddf2b96160e37749ddf72d8ed05
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-16 16:35:50 +0300

    config.php - Add constants

--- functions/config.php
+++ functions/config.php
@@ -1,7 +1,18 @@
 <?php
+const DEFAULT_DIR = 'assets/images';
 const MAX_FILES_ALLOWED = 10;
 const ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png'];
 const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 MB
+const ERROR_MESSAGES = [
+    0 => 'Please choose at least one file for upload',
+    1 => 'No more than 10 files allowed to upload',
+    2 => ' has incorrect format. Allowed formats: jpg, jpeg, png.',
+    3 => ' more than max file size - 5 Mb.'
+];
+const UPLOAD_MESSAGES = [
+    0 => 'Failed to upload images.',
+    1 => 'Images uploaded successfully!'
+];
 
 include_once('helpers.php');
 include_once('validators.php');
