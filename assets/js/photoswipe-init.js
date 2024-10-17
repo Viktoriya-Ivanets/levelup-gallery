c67828899dbbecd0657a9d5fa02f658ff96eec6f
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-17 14:23:58 +0300

    photoswipe-init.js - Include PhotoSwipe files correctly

--- assets/js/photoswipe-init.js
+++ assets/js/photoswipe-init.js
@@ -1,9 +1,9 @@
-import PhotoSwipeLightbox from '../../node_modules/photoswipe/dist/photoswipe-lightbox.esm.js';
+import PhotoSwipeLightbox from '../../lib/photoswipe/photoswipe-lightbox.esm.js';
 
 const lightbox = new PhotoSwipeLightbox({
     gallery: '#gallery-grid',
     children: 'a',
-    pswpModule: () => import('../../node_modules/photoswipe/dist/photoswipe.esm.js')
+    pswpModule: () => import('../../lib/photoswipe/photoswipe.esm.js')
 });
 
 lightbox.init();
