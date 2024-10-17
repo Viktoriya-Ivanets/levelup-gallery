ff3fd041e2ed24951afab24aed2d8d795552c55e
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-17 14:22:35 +0300

    templates/ - Rename to views/, move main_template.php to templates/

--- views/templates/main_template.php
+++ views/templates/main_template.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Gallery</title>
     <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" href="node_modules/photoswipe/dist/photoswipe.css">
+    <link rel="stylesheet" href="lib/photoswipe/photoswipe.css">
 </head>
 
 <body>
@@ -18,12 +18,12 @@
             </div>
         <?php endif; ?>
         <h1>Photo Gallery</h1>
-        <?php include_once('common/form.php'); ?>
-        <?php include_once('layouts/grid_layout.php'); ?>
+        <?php include_once('views/common/form.php'); ?>
+        <?php include_once('views/layouts/grid_layout.php'); ?>
     </div>
     <!-- PhotoSwipe slider HTML structure -->
-    <?php include_once('common/slider.php'); ?>
-    <script src="node_modules/photoswipe/dist/photoswipe-lightbox.esm.js" type="module"></script>
+    <?php include_once('views/common/slider.php'); ?>
+    <script src="lib/photoswipe/photoswipe-lightbox.esm.js" type="module"></script>
     <script src="assets/js/photoswipe-init.js" type="module"></script>
     <script src="assets/js/scripts.js"></script>
 </body>
